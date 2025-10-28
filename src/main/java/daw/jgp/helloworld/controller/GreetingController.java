a13dc730fe6e6cb3a93f3cee19d28aa78c5ec035
Javier García <tu-correo@ejemplo.com>
2025-10-28 18:09:34 +0100

    Modificada la clase GreetingController y el html user-greeting para que si se accede como http://localhost:8080/greeting muestre un usuario por defecto y se accede por http://localhost:8080/greeting?name1=Ximo&name2=David muestre los 2 usuarios que se pasan por el navegador

--- src/main/java/daw/jgp/helloworld/controller/GreetingController.java
+++ src/main/java/daw/jgp/helloworld/controller/GreetingController.java
@@ -4,14 +4,26 @@ import daw.jgp.helloworld.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class GreetingController {
     // Cuando accedamos a esta URL, accederemos a este controlador.
     @GetMapping("/greeting")
-    public String userGreeting(Model model) { // Nombre del controlador
-        model.addAttribute("message", "¡Bienvenido!"); // Envíamos un String message a la plantilla
-        User userObject = new User("Javi", "javi@example.com"); // Accedemos al modelo para crear un objeto
-        model.addAttribute("userObject", userObject); // Envíamos un objeto a la plantilla
+    public String userGreeting(@RequestParam(required = false) String name1,
+                               @RequestParam(required = false) String name2,
+                               Model model) { // Nombre del controlador
+        if (name1 != null || name2 != null) {
+            model.addAttribute("message", "¡Bienvenidos!"); // Envíamos un String message a la plantilla
+            User userObject = new User(name1, name1 + "@example.com"); // Accedemos al modelo para crear un objeto
+            model.addAttribute("userObject", userObject); // Envíamos un objeto a la plantilla
+            User userObject2 = new User(name2, name2 + "@example.com"); // Accedemos al modelo para crear un objeto
+            model.addAttribute("userObject2", userObject2); // Envíamos un objeto a la plantilla
+        }else{
+            model.addAttribute("message", "¡Bienvenido!"); // Envíamos un String message a la plantilla
+            User userObject3 = new User("Javi", "javi@example.com"); // Accedemos al modelo para crear un objeto
+            model.addAttribute("userObject3", userObject3); // Envíamos un objeto a la plantilla
+        }
         return "user-greeting"; // Plantilla que utilizará este controlador
     }
 }
--- src/main/java/daw/jgp/helloworld/controller/helloworldController.java
+++ src/main/java/daw/jgp/helloworld/controller/helloworldController.java
@@ -1,6 +1,5 @@
 package daw.jgp.helloworld.controller;
 
-import daw.jgp.helloworld.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class helloworldController {
     @GetMapping("/helloworld")
-    public String greeting (@RequestParam(required = false) String name1,
-                            @RequestParam(required = false) String name2,
-                            Model model){
-        model.addAttribute("message", "Bienvenidos "+name1+" y "+name2);
-
+    public String greeting (@RequestParam(required = false) String username, Model model){
+        model.addAttribute("message", "Bienvenido "+username);
         return "greeting";
     }
 }
