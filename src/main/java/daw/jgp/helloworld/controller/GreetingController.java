5133ab6fb140ca342a8aa5694318421bb6044573
Javier García <tu-correo@ejemplo.com>
2025-10-28 17:03:02 +0100

    aCreada la clase User y clase GreetingController

---
+++ src/main/java/daw/jgp/helloworld/controller/GreetingController.java
@@ -0,0 +1,17 @@
+package daw.jgp.helloworld.controller;
+
+import daw.jgp.helloworld.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class GreetingController {
+    // Cuando accedamos a esta URL, accederemos a este controlador.
+    @GetMapping("/greeting")
+    public String userGreeting(Model model) { // Nombre del controlador
+        model.addAttribute("message", "¡Bienvenido!"); // Envíamos un String message a la plantilla
+        User userObject = new User("Ximo", "Ximo@example.com"); // Accedemos al modelo para crear un objeto
+        model.addAttribute("userObject", userObject); // Envíamos un objeto a la plantilla
+        return "user-greeting"; // Plantilla que utilizará este controlador
+    }
+}
---
+++ src/main/java/daw/jgp/helloworld/model/User.java
@@ -0,0 +1,12 @@
+package daw.jgp.helloworld.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Setter
+@Getter
+@AllArgsConstructor
+public class User {
+    private String name;
+    private String email;
+}
