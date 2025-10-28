8d0ff59be38ee4e56d2e8b385c7587d468fc1f17
Javier García <tu-correo@ejemplo.com>
2025-10-28 17:36:30 +0100

    Modificado helloworldController para saludar a 2 usuarios a la vez

--- src/main/java/daw/jgp/helloworld/controller/helloworldController.java
+++ src/main/java/daw/jgp/helloworld/controller/helloworldController.java
@@ -9,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class helloworldController {
     @GetMapping("/helloworld")
-    public String greeting(@RequestParam(required = false) String username, Model model){
-        model.addAttribute("message", "Hola " + username);
+    public String greeting (@RequestParam(required = false) String name1,
+                            @RequestParam(required = false) String name2,
+                            Model model){
+        model.addAttribute("message", "Bienvenidos "+name1+" y "+name2);
+
         return "greeting";
     }
 }
