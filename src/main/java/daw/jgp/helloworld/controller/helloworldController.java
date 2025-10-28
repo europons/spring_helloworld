0da102debc35006d680615063cbc52654653fae2
Javier García <tu-correo@ejemplo.com>
2025-10-28 17:17:22 +0100

    Creada la clase helloworldController y el greeting.html

---
+++ src/main/java/daw/jgp/helloworld/controller/helloworldController.java
@@ -0,0 +1,16 @@
+package daw.jgp.helloworld.controller;
+
+import daw.jgp.helloworld.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class helloworldController {
+    @GetMapping("/helloworld")
+    public String greeting(@RequestParam(required = false) String username, Model model){
+        model.addAttribute("message", "Hola " + username);
+        return "greeting";
+    }
+}
